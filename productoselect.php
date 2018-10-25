ec72c60f5954e6c41690ac43ca46554d4f88e41c
paramiomarcelo <paramiomarcelo@gmail.com>
2018-10-25 00:17:51 -0300

    productoelegido

--- productoselect.php
+++ productoselect.php
@@ -30,20 +30,50 @@
 					<?php
 					include("sql/conexion.php");
 					include("sql/select.php");
-					while($registro = $consulta->fetch_assoc()){
-					if ($_REQUEST['c']==$registro['id_producto']){?>
-					<a href="" class="link"><div class="gen">
+					$sel="SELECT * FROM productos WHERE id_producto='".$_REQUEST['c']."'";
+					$consul = $conexion->query($sel);
+					
+					while($registro = $consul->fetch_assoc()){
+					if ($_REQUEST['c']==$registro['id_producto']){
+					?>
+					<div class="prodtodo">
+					<div class="gen">
 					<img class="imgprod" src="img/prod/<?php echo $registro['imagen']; ?>">
+					</div>
 					<div class="productosinfo">
-					<h2 class="nombreprod"><?php echo $registro['nombre'];?></h2>
-					<h4>PRECIO $<?php echo $registro['precio']; ?></h4>
-					<h4>Descripcion del producto: <?php echo $registro['descripcion']; ?></h4>		
-					<h4>STOCK: <?php echo $registro['stock']; ?></h4></div>
-					</div></a><?php
+					<p class="nombreprod"><?php echo $registro['nombre'];?></p>
+					<p class="stock">STOCK : <?php echo $registro['stock']; ?></p>
+					<p class="precio">PRECIO $<?php echo $registro['precio']; ?></p>
+					<input type="submit" class="botoncompra" name="comprar" value="COMPRAR">
+					</div>
+					</div>
+					<div class="desc">
+					<p class="tdesc">Descripcion del producto</p>
+					<hr  noshade="noshade" size="1" color="black" />
+					<p class="infodesc"><?php echo $registro['descripcion']; ?></p>
+					</div>
+					<div class="otrosprods">
+					<?php
+					$selcat="SELECT id_producto,nombre,descripcion,stock,precio,imagen FROM productos WHERE tipo='".$registro['tipo']."' LIMIT 5";
+					$select = $conexion->query($selcat);
+					
+					while($registrocat = $select->fetch_assoc()){
+					?>
+					<img class="" src="img/prod/<?php echo $registrocat['imagen']; ?>">
+					<p class=""><?php echo $registrocat['nombre'];?></p>
+					<p class="">PRECIO $<?php echo $registrocat['precio']; ?></p>
+					<p class="">STOCK : <?php echo $registrocat['stock']; ?></p>
+
+					
+							
+					</div>
+					<?php
+					}
 						}
 					}
 					?>
-
+					
+				
 				
 	<?php include("includes/footer.html"); ?>
 		
