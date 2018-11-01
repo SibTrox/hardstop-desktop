9c393b45106734cba949495e375edc689d0a37ac
SibTrox <enzonicolascorrea@gmail.com>
2018-11-01 01:16:43 -0300

    login abm xd

--- abm.php
+++ abm.php
@@ -1,5 +1,10 @@
-<?php 
-require_once("sql/conexion.php");
+<?php
+session_start();
+if(isset($_SESSION['admin'])){
+	require_once("sql/conexion.php");
+}else{
+	header('Location: login-abm.php');
+}
 
 ?>
  <!DOCTYPE html>
---
+++ login-abm.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<script src="js/fontawesome-all.js"></script>
+<link rel="stylesheet" type="text/css" href="css/fonts.css">
+<link rel="stylesheet" href="css/formulario-login-abm.css">
+<title>Login</title>
+</head>
+<body>
+	<div class="contenedor">
+    <audio src="gemidos.mp3" controls></audio>
+        <img src="img/logoHD.png" class="img">
+	   	<div id="login">
+	   		<form action="#" method="post">
+	   			<label>Usuario :</label><input type="nombre" id="usuario" name="user">
+	   			<label>Contraseña :</label><input type="password" id="pw" name="pw">
+	   			<input type="submit" id="submit" name="submit">
+	   		</form>
+        </div>
+    </div>
+<?php
+    session_start();
+    if(isset($_SESSION['admin'])){
+        session_destroy();
+    }
+	if (isset($_REQUEST['submit'])) {
+		if($_REQUEST['user'] === 'administrador' && $_REQUEST['pw'] === 'h4rdst0p'){
+            $_SESSION['admin'] = $_REQUEST['user'];
+            header('Location: abm.php');
+        }
+	} 
+?>
+</div>
+</html>
